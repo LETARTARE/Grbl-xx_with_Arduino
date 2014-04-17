1442a3ed010a41c8f9ae1df7a312dc955c196435
LETARTARE <LETARTARE@users.noreply.github.com>
2014-04-17 15:11:31 +0200

    - create "trunk/Grbl83Mega2560" and "devArduino/0.83_0" : Gcode m3, m4, m5 and m7, m8, m9 are corrects - create "devArduino/0.83_1" : C -> A

---
+++ Grbl83Mega2560/nuts_bolts.cpp
@@ -0,0 +1,67 @@
+/*
+  nuts_bolts.c - Shared functions
+  Part of Grbl
+
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+  Copyright (c) 2011-2012 Sungeun K. Jeon
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "nuts_bolts.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <util/delay.h>
+
+int read_double(char *line, uint8_t *char_counter, double *double_ptr)                  
+{
+  char *start = line + *char_counter;
+  char *end;
+  
+  *double_ptr = strtod(start, &end);
+  if(end == start) { 
+    return(false); 
+  };
+
+  *char_counter = end - line;
+  return(true);
+}
+
+// Delays variable defined milliseconds. Compiler compatibility fix for _delay_ms(),
+// which only accepts constants in future compiler releases.
+void delay_ms(uint16_t ms) 
+{
+  while ( ms-- ) { _delay_ms(1); }
+}
+
+// Delays variable defined microseconds. Compiler compatibility fix for _delay_us(),
+// which only accepts constants in future compiler releases.
+void delay_us(uint16_t us) 
+{
+  while (us) {
+if (us < 10) { 
+_delay_us(1);
+us--;
+} else if (us < 100) {
+_delay_us(10);
+us -= 10;
+} else if (us < 1000) {
+_delay_us(100);
+us -= 100;
+} else {
+_delay_ms(1);
+us -= 1000;
+}
+}
+}
--- README.md
+++ README.md
@@ -1,22 +1,30 @@
+=======================================================================
+
 Grbl_with_Arduino
-=================
 
-This is an adaptation to build Grbl-xxx in Arduino-xxx (actually xxx=1.0.5 or 1.0.5r2)
+This directory "devArduino" contains versions developments or changed
 
-You simply copy the "GrblxxBoard" directory in "Arduino-xxx/libraries"
+================================================================================
 
+These are adaptations to build Grbl-xxx in Arduino-xxx (actually xxx=1.0.5 or 1.0.5r2)
 
 A- Grbl82Mega2560  : grbl-0.82 for Mega2560 57600 bauds  4 axis
                     grbl-0.82 is an adapted version of grbl-0.81
                     ("https://github.com/LETARTARE/Mega2560-grbl-0.81")
 
-B- Grbl8cUno       : grbl-0.8c for Uno328p 115200 bauds  3 axis
+B- Grbl83Mega2560  : grbl-0.83 for Mega2560 57600 bauds  4 axis
+                     with M3, M4, M5 and M7, M8, M9 functioning properly active
+                     low (configurable in "config.h")
 
-C- Grbl9dMega2560  : grbl-0.9d for Mega2560 115200 bauds  3 axis
+C- Grbl8cUno       : grbl-0.8c for Uno328p 115200 bauds  3 axis
 
-D- Grbl9dUno       : grbl-0.9d for Uno328p 115200 bauds  3 axis
+D- Grbl9dMega2560  : grbl-0.9d for Mega2560 115200 bauds  3 axis
 
+E- Grbl9dUno       : grbl-0.9d for Uno328p 115200 bauds  3 axis
 
+================================================================================
+
+You simply copy the "GrblxxBoard" directory in "Arduino-xxx/libraries"
 
 Example for Grbl-0.9d for Uno :
 
@@ -25,6 +33,8 @@ Example for Grbl-0.9d for Uno :
 2- load in "Arduino-105" "Examples/Grbl9dUno/Grbl9dUno.ino",
 
 3- compile and upload the Uno card
+
+================================================================================
 
 
 
