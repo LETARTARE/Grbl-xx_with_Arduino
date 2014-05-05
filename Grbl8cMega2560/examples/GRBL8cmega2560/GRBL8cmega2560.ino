310840c1c838f06e5de812176317a85cd75a4350
LETARTARE <LETARTARE@users.noreply.github.com>
2014-05-05 16:06:04 +0200

    - upload "Grbl8cMega2560" (official)

---
+++ Grbl8cMega2560/examples/GRBL8cmega2560/GRBL8cmega2560.ino
@@ -0,0 +1,32 @@
+/*
+    Grbl_8cMega2560.ino BY LETARTARE
+    This file is an adaptation of Grbl-0.9d work with Arduino-xxx.
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
+/*
+
+	Supported hardware from config.h
+		Arduino Mega 2560 / 115200 bauds  (3 axis)
+    Supported sofware
+        grbl 0.8c 
+
+*/
+
+#include <main.h>
+
+void setup(){
+	main();
+}
+
+void loop(){}
